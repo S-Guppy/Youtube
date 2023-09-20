b73a3ca9b344c13cfc7dfac4e1154058cd7bb2d7
S-Guppy <132241375+S-Guppy@users.noreply.github.com>
2023-09-20 23:29:53 +1200

    finishing touches and changed the header

--- static/js/js.js
+++ static/js/js.js
@@ -1,15 +1,4 @@
 /* Toggle between adding and removing the "responsive" class to topnav when the user clicks on the icon */
-
-/*function myFunction() {
-    var x = document.getElementById("myTopnav");
-    if (x.className === "topnav") {
-      x.className += " responsive";
-    } else {
-      x.className = "topnav";
-    }
-  }
-  */
-
 function myFunction() {
   var x = document.getElementById("myTopnav");
   if (x.className === "topnav") {
@@ -18,4 +7,3 @@ function myFunction() {
     x.className = "topnav";
   }
 }
- 
